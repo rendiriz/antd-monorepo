33fc23e72602f6ddc0778ef2c887c1b51ffb00f4
Rendi Rizki <rendirizkir@gmail.com>
2023-10-18 12:53:45 +0700

    chore: setup

---
+++ libs/next/landing/utils/README.md
@@ -0,0 +1,7 @@
+# next/landing/utils
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test next/landing/utils` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/shared/tailwind/tailwind.config.js
@@ -0,0 +1,30 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const plugin = require('tailwindcss/plugin');
+
+module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+  content: [],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-lato)', ...defaultTheme.fontFamily.sans],
+        serif: ['var(--font-lora)', ...defaultTheme.fontFamily.serif],
+        mono: [...defaultTheme.fontFamily.mono],
+        body: ['var(--font-inter)'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    plugin(function ({ addBase, theme }) {
+      addBase({
+        body: { fontFamily: theme('fontFamily.body') },
+      });
+    }),
+  ],
+};

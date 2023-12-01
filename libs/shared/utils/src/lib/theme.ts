690e11516cefd4df5723f5beeaad708b692d4d9a
Rendi Rizki <rendirizkir@gmail.com>
2023-12-02 00:17:27 +0700

    chore(admin): setup

--- libs/shared/utils/src/lib/theme.ts
+++ libs/shared/utils/src/lib/theme.ts
@@ -3,6 +3,7 @@ import type { ThemeConfig } from 'antd';
 export const theme: ThemeConfig = {
   token: {
     fontFamily: 'var(--font-inter)',
+    fontSize: 16,
     colorPrimary: '#7cb305',
   },
 };
